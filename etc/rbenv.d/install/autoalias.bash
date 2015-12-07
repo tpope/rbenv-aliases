197fec2d1a61a72c6035a0533f72938ace8cb213
Jason Karns <jason@karns.name>
2015-12-07 15:47:26 -0500

    Unify case statement
    The two case statements are meant to allow a single version to be
    auto-aliased twice (once with the minimum patch segment removed, and
    again with the maximum patch segment removed)
    
    Due to the idempotent behavior of `alias <version> --auto`, running the
    same alias --auto twice has no ill effect. Thus, if the shortest-match
    and longest-match versions are identical, there's no harm in simply
    running alias --auto twice. This reduces a bunch of redundant code that
    isn't completely clear in its purpose. (and ran the risk of
    inadvertently being merged into a single case with 2 branches, which
    would have actually been a regression)

--- etc/rbenv.d/install/autoalias.bash
+++ etc/rbenv.d/install/autoalias.bash
@@ -3,10 +3,10 @@ after_install autoalias
 autoalias() {
   if [ "$STATUS" = 0 ]; then
     case "$VERSION_NAME" in
-      *[0-9]-*-*) rbenv alias "${VERSION_NAME%-*}" --auto 2>/dev/null || true ;;
-    esac
-    case "$VERSION_NAME" in
-      *[0-9]-*) rbenv alias "${VERSION_NAME%%-*}" --auto 2>/dev/null || true ;;
+    *[0-9]-*)
+      rbenv alias "${VERSION_NAME%-*}" --auto 2>/dev/null || true
+      rbenv alias "${VERSION_NAME%%-*}" --auto 2>/dev/null || true
+      ;;
     esac
   fi
 }
